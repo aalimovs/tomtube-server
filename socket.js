747cd5c7e3054d6df9478d365da86af412958365
Unknown <tom.c.gray@gmail.com>
2018-04-26 00:24:29 +0100

    basic working room

--- socket.js
+++ socket.js
@@ -1,79 +1,104 @@
 const _ = require('lodash');
+const Uuid = require('uuid/v4');
 
 module.exports = (args) => {
     var app = require('express')();
     var server = require('http').Server(app);
     var io = require('socket.io')(server);
-    
+
     const rooms = [];
 
     server.listen(4001);
 
-    io.use(function(socket, next){
-        console.log("Query: ", socket.handshake.query);
-        // return the result of next() to accept the connection.
-        return next();
-        // call next() with an Error if you need to reject the connection.
-        // next(new Error('Authentication error'));
-    });
-
     io.on('connection', function (socket) {
         console.log('SOCKET - new connection');
-        
-        console.log('***************', socket.handshake.headers);
+        socket._data = {
+            type: socket.handshake.query.type,
+            room_code: socket.handshake.query.room_code,
+            uuid: Uuid(), 
+        };
 
-        const socketData = prepareSocket(socket);
-
-        console.log('SOCKET - socketData', socketData);
-        console.log('socketData.type', socketData.type);
-        if (socketData.type === 'player') {
-            console.log('SOCKET - new player - creating room');
-            const room = createNewRoom();
-            console.log(`SOCKET - generated room with code ${room.code}`);
-            room.sockets.push(socket);
-            rooms.push(room);
-            console.log('SOCKET - total rooms', rooms);
-            emitWelcome(socket, room);
-        } else if (socketData.type === 'user') {
-            console.log('SOCKET - new user - wants to join room:', socketData.room_code);
-            
-            const room = findRoomForCode(socketData.room_code);
-            if (room) {
-                console.log('SOCKET - user found room for code:', socketData.room_code, '- joining room');
+        console.log('socket._data', socket._data);
+        if (socket.handshake.headers.referer.includes('player')) {
+            console.log('SOCKET - new connection is a player');
+            if (socket._data.room_code && rooms.length >= 1) { // only add them to a room if they have a room code AND there are rooms
+                console.log('SOCKET - player WITH room code - adding to room');
+                const room = findRoomForCode(socket._data.room_code, rooms);
                 room.sockets.push(socket);
             } else {
-                console.log('SOCKET - user - no room found for code:', socketData.room_code);
+                console.log('SOCKET - player, no room code - creating room');
+                const room = createNewRoom();
+                socket._data.room_code = room.code;
+                console.log(`SOCKET - generated room with code ${room.code}`);
+                room.sockets.push(socket);
+                rooms.push(room);
+                emitWelcome(socket, room);
+            }
+
+        } else if (socket.handshake.headers.referer.includes('user')) {
+            console.log('SOCKET - new user - wants to join room:', socket._data.room_code);
+
+            const room = findRoomForCode(socket._data.room_code, rooms);
+            if (room) {
+                console.log('SOCKET - user found room for code:', socket._data.room_code, '- joining room');
+                room.sockets.push(socket);
+            } else {
+                console.log('SOCKET - user - no room found for code:', socket._data.room_code);
             }
         }
+
+        socket.on('disconnect', () => {
+            const room = findRoomForCode(socket._data.room_code, rooms);
+            if (socket._data.type === 'player') {
+                console.log('SOCKET - disconnected user was a player, self destructing room');
+                room.selfDestruct(rooms);
+                console.log('SOCKET - room self descructed, rooms left:', rooms);
+            } else if (socket._data.type === 'user') {
+                // user disconnected - remove it from its room
+                if (room) {
+                    room.removeSocket(socket._data.uuid);
+                }
+            }
+        });
     });
 
     return {
         emit: (code, key, data) => {
-            const room = findRoomForCode(code);
+            const room = findRoomForCode(code, rooms);
             room.emit(key, data);
+        },
+        getRooms: () => {
+            return rooms;
+        },
+        getRoom: (code) => {
+            return findRoomForCode(code, rooms);
         },
     };
 }
 
-const findRoomForCode = code => rooms.find(r => r.code === code);
+const findRoomForCode = (code, rooms) => rooms.find(r => r.code === code);
 
 const createNewRoom = () => {
+    const code = generateRoomCode();
     const sockets = [];
+    const playlist = [];
+
     return {
-        code: generateRoomCode(),
+        code,
         sockets,
+        playlist,
         emit: (key, data) => {
             sockets.forEach(socket => socket.emit(key, data));
         },
+        selfDestruct: function (rooms) {
+            sockets.forEach(socket => socket.disconnect());
+            _.remove(rooms, r => r.code === code);
+        },
+        removeSocket: function (socketUuid) {
+            const removedSockets = _.remove(sockets, s => s.uuid === socketUuid);
+            removedSockets.forEach(s => s.disconnect());
+        },
     };
-};
-
-const prepareSocket = socket => {
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-
-    return socket.handshake.query;
 };
 
 const generateRoomCode = () => {
@@ -81,7 +106,7 @@ const generateRoomCode = () => {
 
     const chars = [];
 
-    for(let i = 0; i < 4; i++) {
+    for (let i = 0; i < 4; i++) {
         chars.push(possibleChars[_.random(possibleChars.length)]);
     }
 
